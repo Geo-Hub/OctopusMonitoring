7fe3eb6fc50cc991679f69ba2de2efdb0d7b29be
Jonathan Fisher <jonathan@fiskur.eu>
2016-09-05 09:43:17 +0100

    initial commit

---
+++ app/src/main/java/org/blueventures/octopusmonitoring/SplashActivity.java
@@ -0,0 +1,48 @@
+package org.blueventures.octopusmonitoring;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SplashActivity extends AppCompatActivity {
+
+  private View rootView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash);
+    rootView = findViewById(R.id.root_view);
+  }
+
+  @Override
+  protected void onPostCreate(Bundle savedInstanceState) {
+    super.onPostCreate(savedInstanceState);
+    hide();
+
+    new CountDownTimer(3000, 1000) {
+
+      public void onTick(long millisUntilFinished) {
+        //
+      }
+
+      public void onFinish() {
+        startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+        finish();
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+      }
+    }.start();
+  }
+
+  private void hide() {
+    ActionBar actionBar = getSupportActionBar();
+    if (actionBar != null) {
+      actionBar.hide();
+    }
+    rootView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+  }
+
+}
